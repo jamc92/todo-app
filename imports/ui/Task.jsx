c6a4565e84e0e5b0525d3268e89005e393f9ba1b
Javier Madrid <javiermadrid19@hotmail.com>
2016-06-11 06:43:01 -0400

    Update & remove features for tasks list

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,39 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 // Task component - represents a single to-do item
 export default class Task extends Component {
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked }
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    // Give a class a differente className when they're checked as done
+    // so that we can style them nice in CSS
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
